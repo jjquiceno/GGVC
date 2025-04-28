318ef40f2c016f1737c1307780c3f9894c19c69d
jjquiceno <quicenolondonoj@gmail.com>
2025-04-28 16:20:35 -0500

    librerias de font-awesome y header

--- my-react-app/src/components/header.jsx
+++ my-react-app/src/components/header.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import './header.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 export const Header = () => {
   return (
     <>
-        <h1 className='text-3xl font-bold underline'>
-            este es el header
-        </h1>
+      <div className='bg-[rgb(233,237,201)] contenedor text-black flex justify-between items-center pl-4'>
+        <div className=''>
+          <FontAwesomeIcon icon={faBars} size='2x'/>
+        </div>
+        <div className=''>
+          <h1 className='text-3xl font-bold'>My React App</h1>
+        </div>
+        <div className='img-container'>
+          <div className='imagen'>
+
+          </div>
+
+        </div>
+      </div> 
     </>
   )
 }
--- my-react-app/src/pruebas.jsx
+++ my-react-app/src/pruebas.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import './pruebas.css'
 
 import { Header } from './components/header.jsx'
 
