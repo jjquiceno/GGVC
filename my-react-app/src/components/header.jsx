1f1bbc7efa96b229653f20bbab2886d0f6f5d9da
jjquiceno <quicenolondonoj@gmail.com>
2025-04-27 19:46:21 -0500

    componente de prueba

---
+++ my-react-app/src/components/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Pa = () => {
+  return (
+    <>
+        <h1 className='text-3xl font-bold underline'>
+            primer componente
+        </h1>
+    </>
+  )
+}
