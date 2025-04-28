f8649b7e2a82e60db95e3b4f849e3109585e510d
Isabella Gonzalez Vasquez <isabellagonzalez155g@gmail.com>
2025-04-27 20:26:20 -0500

    Inicia componente FormLogin

---
+++ my-react-app/src/components/formLogin.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const FormLogin = () => {
+  return (
+    <>
+        <h1 className='text-3xl font-bold underline'>
+            este es el form
+        </h1>
+    </>
+  )
+}
