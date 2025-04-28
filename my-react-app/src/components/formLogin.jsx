adcb5b773972592d14627ed5bbf841a1a7994ead
Isabella Gonzalez Vasquez <isabellagonzalez155g@gmail.com>
2025-04-27 20:38:28 -0500

    Componente FormLogin

--- my-react-app/src/components/formLogin.jsx
+++ my-react-app/src/components/formLogin.jsx
@@ -2,10 +2,33 @@ import React from 'react'
 
 export const FormLogin = () => {
   return (
-    <>
-        <h1 className='text-3xl font-bold underline'>
-            este es el form
-        </h1>
-    </>
+    <div className="login">
+      <h1>INICIAR SESIÓN</h1>
+
+      <br /><br />
+
+      <form action="">
+        <div className="input-icon">
+          <input type="text" id="usuario" placeholder="Usuario" required />
+          <i className="fas fa-user"></i>
+        </div>
+
+        <br /><br />
+
+        <div className="input-icon">
+          <input type="password" id="password" name="password" placeholder="Contraseña" required />
+          <i className="fas fa-lock"></i>
+        </div>
+
+        <br /><br />
+
+        <button type="submit">Ingresar</button>
+
+        <br /><br />
+
+        <p>Olvidé mi <a href="html/#">Contraseña</a></p>
+        <p>Aún no tengo una cuenta <a href="../html/registro.html">Registrarse</a></p>
+      </form>
+    </div>
   )
 }
