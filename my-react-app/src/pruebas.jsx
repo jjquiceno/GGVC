149517a208783b841289166b9be3ac91421f9897
Isabella Gonzalez Vasquez <isabellagonzalez155g@gmail.com>
2025-05-01 14:58:36 -0500

    Inicio formRegister

--- my-react-app/src/pruebas.jsx
+++ my-react-app/src/pruebas.jsx
@@ -3,13 +3,18 @@ import './App.css'
 import './pruebas.css'
 
 import { Header } from './components/header.jsx'
+import { FormLogin } from './components/formLogin.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <div className='w-full'></div>
+    <h1>Header</h1>
       <Header/>
+      <h1>formLogin</h1>
+      <FormLogin/>
     </>
   )
 }
