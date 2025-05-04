4ced15cde446300844f9e791af8a38463d8979d0
jjquiceno <quicenolondonoj@gmail.com>
2025-05-04 18:16:27 -0500

    icono

--- my-react-app/src/components/Menuh.jsx
+++ my-react-app/src/components/Menuh.jsx
@@ -47,3 +47,46 @@ export const Menu = () => {
     </div>
   );
 };
+export const MenuLeft = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <div className="menu">
+      <div className="menuitem" onClick={toggleMenu}>
+        <FontAwesomeIcon icon={faBars} size="2x" />
+      </div>
+      <div className={`menu-float-left ${menuOpen ? 'active' : ''}`}>
+        <div className='menu-float-items'>
+            <div className="cerrar" onClick={toggleMenu}>
+                <FontAwesomeIcon icon={faXmark} size="2x" />
+            </div>
+            <div className="separador-menu"></div>
+            <div className='opciones'>
+                <nav className='opciones1'>
+                    <ul>
+                        <li><a href=""><FontAwesomeIcon icon={faHouse}/> Inicio</a></li>
+                        <li><a href=""><FontAwesomeIcon icon={faCow}/> Ganado</a></li>
+                        <li><a href=""><FontAwesomeIcon icon={faSeedling}/> Cultivos</a></li>
+                        <li><a href=""><FontAwesomeIcon icon={faXmarksLines}/> Potreros</a></li>
+                        <li><a href=""><FontAwesomeIcon icon={faDollarSign}/> Nómina</a></li>
+                        <li><a href=""><FontAwesomeIcon icon={faBookOpen}/> Manuales</a></li>
+                    </ul>
+                </nav>
+                <nav className='opciones2'>
+                    <div className="separador-menu"></div>
+                    <ul>
+                        <li><a href=""><FontAwesomeIcon icon={faUserPen}/> Ajustes</a></li>
+                        <li><a href=""><FontAwesomeIcon icon={faGear}/> Contácto</a></li>
+                    </ul>
+                </nav>
+            </div> 
+        </div>
+        
+      </div>
+    </div>
+  );
+};
