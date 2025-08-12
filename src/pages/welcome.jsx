636122189866a1883081a3c4f1f07e2b510393dd
jjquiceno <quicenolondonoj@gmail.com>
2025-08-12 08:56:21 -0500

    correccion vista del welcome

--- src/pages/welcome.jsx
+++ src/pages/welcome.jsx
@@ -20,8 +20,8 @@ function WelcomePage() {
     <div className="flex justify-center items-center w-full h-screen bg-[#fffdef]">
       <Header nav={<Menu/>} text={`Bienvenido ${decoded.nombre}`} img={"/img/bienvenidosGanado.png"}/>
 
-      <div className="mt-[25vh] pt-[25vh] pb-[5vh] overflow-auto h-[75vh] w-full md:h-[70%] md:pt-[0] md:mt-[30vh] flex flex-col gap-10 justify-center items-center md:grid md:grid-cols-[2fr_1fr] md:grid-rows-1 md:items-center">
-        <div className="mt-[80vh] flex flex-col h-fit gap-20 md:flex-row md:justify-around w-[100%] md:h-[90%] md:ml-[20%] md:items-center ">
+      <div className="border mt-[25vh] pt-[25vh] pb-[5vh] overflow-auto h-[75vh] w-full md:h-[70%] md:pt-[0] md:mt-[30vh] flex flex-col gap-10 justify-center items-center md:grid md:grid-cols-[2fr_1fr] md:grid-rows-1 md:items-center">
+        <div className="border mt-[80vh] md:mt-[0] flex flex-col h-fit gap-20 md:flex-row md:justify-around w-[100%] md:h-[90%] md:ml-[20%] md:items-center ">
           <ButtonAreas img="/img/ganadosobrioia.jpg" text={"Ganado"} ruta="/ganado" />
           <ButtonAreas img="/img/nomina.jpg" text={"Nomina"} ruta="#"/>
           <div className="m-auto w-[80%] h-[70%]bg-gradient-to-r from-[#ccd5ae] to-[#e9edc9] border-4 border-[#2b3701] rounded-[10px] flex justify-center items-center flex-col shadow-[-10px_0px_10px_-2px_rgba(0,0,0,0.164)]">
